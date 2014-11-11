53fbd94af725f353c1a69c303a7df7bd6cec4185
Holden Rehg <holdenrehg@gmail.com>
2014-11-11 02:29:04 -0600

    Load list of apps in manage apps

--- app/src/main/java/com/blue_stingray/healthy_life_app/activity/MainActivity.java
+++ app/src/main/java/com/blue_stingray/healthy_life_app/activity/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.MenuItem;
--- app/src/main/java/com/blue_stingray/healthy_life_app/fragment/ManageGoalsFragment.java
+++ app/src/main/java/com/blue_stingray/healthy_life_app/fragment/ManageGoalsFragment.java
@@ -2,6 +2,9 @@ package com.blue_stingray.healthy_life_app.fragment;
 
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.app.ProgressDialog;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -9,12 +12,26 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import com.blue_stingray.healthy_life_app.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ManageGoalsFragment extends Fragment {
+
+    private ProgressDialog loadingDialog;
+    private ListView appList;
+    private ArrayList<String> apps;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_manage_goals, container,false);
+        View view = inflater.inflate(R.layout.fragment_manage_goals, container,false);
+        appList = (ListView) view.findViewById(R.id.apps);
+        loadingDialog = ProgressDialog.show(getActivity(), "", "Loading Applications...", true);
+        new CreateList().start();
         setHasOptionsMenu(true);
         return view;
     }
@@ -40,4 +57,32 @@ public class ManageGoalsFragment extends Fragment {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    private void createList() {
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                final ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, apps);
+                appList.setAdapter(adapter);
+            }
+        });
+    }
+
+    private class CreateList extends Thread {
+        @Override
+        public void run() {
+            try {
+                final PackageManager pm = getActivity().getPackageManager();
+                apps = new ArrayList<>();
+                List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+                for (ApplicationInfo packageInfo : packages) {
+                    apps.add(packageInfo.loadLabel(pm).toString());
+                }
+            } finally {
+                createList();
+                loadingDialog.dismiss();
+            }
+        }
+    }
+
 }
