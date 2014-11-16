b17c91dcb22566e56d13b4a3afebc4a8c5a78e8b
Holden Rehg <holdenrehg@gmail.com>
2014-11-15 19:08:54 -0600

    Add app, goal type, and time limit spinners

--- app/src/main/java/com/blue_stingray/healthy_life_app/fragment/CreateGoalFragment.java
+++ app/src/main/java/com/blue_stingray/healthy_life_app/fragment/CreateGoalFragment.java
@@ -1,10 +1,16 @@
 package com.blue_stingray.healthy_life_app.fragment;
 
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 import com.blue_stingray.healthy_life_app.R;
 
 public class CreateGoalFragment extends Fragment {
@@ -12,6 +18,29 @@ public class CreateGoalFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_create_goal, container,false);
         getActivity().setTitle("Create Goal");
+
+        // app spinner
+        Spinner appSpinner = (Spinner) view.findViewById(R.id.app_spinner);
+
+        // goal type spinner
+        Spinner goalTypeSpinner = (Spinner) view.findViewById(R.id.goal_type_spinner);
+        ArrayAdapter<CharSequence> goalTypeAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.goal_type, android.R.layout.simple_list_item_1);
+        goalTypeSpinner.setAdapter(goalTypeAdapter);
+
+        // time limit spinner
+        Spinner timeLimitSpinner = (Spinner) view.findViewById(R.id.time_spinner);
+        ArrayAdapter<CharSequence> timeLimitAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.time_limit, android.R.layout.simple_list_item_1);
+        timeLimitSpinner.setAdapter(timeLimitAdapter);
+
+        // create goal on click
+        Button button = (Button) view.findViewById(R.id.create_goal);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "todo", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return view;
     }
 }
