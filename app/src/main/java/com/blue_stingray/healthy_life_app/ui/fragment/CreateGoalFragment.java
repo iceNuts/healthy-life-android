71a4265a5de8ec4d23fb7ffc57cb47d9bc02c49d
Holden Rehg <holdenrehg@gmail.com>
2014-11-25 15:57:49 -0600

    Update create goals with day sliders

--- app/src/main/java/com/blue_stingray/healthy_life_app/ui/fragment/CreateGoalFragment.java
+++ app/src/main/java/com/blue_stingray/healthy_life_app/ui/fragment/CreateGoalFragment.java
@@ -1,6 +1,5 @@
 package com.blue_stingray.healthy_life_app.ui.fragment;
 
-import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +12,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.blue_stingray.healthy_life_app.App;
 import com.blue_stingray.healthy_life_app.R;
-import com.blue_stingray.healthy_life_app.model.Application;
 import com.blue_stingray.healthy_life_app.net.form.validation.FormValidationManager;
 import com.blue_stingray.healthy_life_app.net.RestInterface;
 import com.blue_stingray.healthy_life_app.net.form.FormSubmitClickListener;
@@ -32,12 +30,6 @@ public class CreateGoalFragment extends RoboFragment {
 
     @InjectView(R.id.app_spinner)
     private Spinner appSpinner;
-
-    @InjectView(R.id.time_seek_bar)
-    private SeekBar timeLimitSeekBar;
-
-    @InjectView(R.id.time)
-    private TextView currentTime;
 
     @InjectView(R.id.create_goal)
     private Button createGoalButton;
@@ -63,12 +55,31 @@ public class CreateGoalFragment extends RoboFragment {
     @InjectView(R.id.sunday)
     private TextView sunday;
 
+    @InjectView(R.id.monday_seek_bar)
+    private SeekBar mondaySeekBar;
+
+    @InjectView(R.id.tuesday_seek_bar)
+    private SeekBar tuesdaySeekBar;
+
+    @InjectView(R.id.wednesday_seek_bar)
+    private SeekBar wednesdaySeekBar;
+
+    @InjectView(R.id.thursday_seek_bar)
+    private SeekBar thursdaySeekBar;
+
+    @InjectView(R.id.friday_seek_bar)
+    private SeekBar fridaySeekBar;
+
+    @InjectView(R.id.saturday_seek_bar)
+    private SeekBar saturdaySeekBar;
+
+    @InjectView(R.id.sunday_seek_bar)
+    private SeekBar sundaySeekBar;
+
     @Inject
     private RestInterface rest;
 
     private FormValidationManager validationManager;
-
-    private HashMap<Integer, Boolean> days;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -81,7 +92,6 @@ public class CreateGoalFragment extends RoboFragment {
         getActivity().setTitle(R.string.title_create_goal);
 
         validationManager = new FormValidationManager();
-        days = new HashMap<>();
 
         // app spinner
         ArrayList<String> keys = new ArrayList<>(((App) getActivity().getApplication()).appCache.snapshot().keySet());
@@ -89,24 +99,15 @@ public class CreateGoalFragment extends RoboFragment {
         ArrayAdapter<String> appAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, keys);
         appSpinner.setAdapter(appAdapter);
 
-        timeLimitSeekBar.setOnSeekBarChangeListener(new TimeLimitSeekBarListener());
         createGoalButton.setOnClickListener(new CreateGoalButtonListener());
 
-        monday.setOnClickListener(new DayPickerListener());
-        tuesday.setOnClickListener(new DayPickerListener());
-        wednesday.setOnClickListener(new DayPickerListener());
-        thursday.setOnClickListener(new DayPickerListener());
-        friday.setOnClickListener(new DayPickerListener());
-        saturday.setOnClickListener(new DayPickerListener());
-        sunday.setOnClickListener(new DayPickerListener());
-
-        setDay(monday, R.color.white, R.color.blue_primary);
-        setDay(tuesday, R.color.white, R.color.blue_primary);
-        setDay(wednesday, R.color.white, R.color.blue_primary);
-        setDay(thursday, R.color.white, R.color.blue_primary);
-        setDay(friday, R.color.white, R.color.blue_primary);
-        setDay(saturday, R.color.white, R.color.blue_primary);
-        setDay(sunday, R.color.white, R.color.blue_primary);
+        mondaySeekBar.setOnSeekBarChangeListener(new TimeLimitSeekBarListener());
+        tuesdaySeekBar.setOnSeekBarChangeListener(new TimeLimitSeekBarListener());
+        wednesdaySeekBar.setOnSeekBarChangeListener(new TimeLimitSeekBarListener());
+        thursdaySeekBar.setOnSeekBarChangeListener(new TimeLimitSeekBarListener());
+        fridaySeekBar.setOnSeekBarChangeListener(new TimeLimitSeekBarListener());
+        saturdaySeekBar.setOnSeekBarChangeListener(new TimeLimitSeekBarListener());
+        sundaySeekBar.setOnSeekBarChangeListener(new TimeLimitSeekBarListener());
     }
 
     private class CreateGoalButtonListener extends FormSubmitClickListener {
@@ -119,9 +120,8 @@ public class CreateGoalFragment extends RoboFragment {
         protected void submit() {
 
             // TODO
-            ArrayList<Integer> selectedDays = getSelectedDays();
-            int hours = timeLimitSeekBar.getProgress() + 1;
             int appId = ((App) getActivity().getApplication()).appCache.get((String) appSpinner.getSelectedItem()).id;
+            HashMap<Integer, Integer> dayMap = getDayHours();
 
             Toast.makeText(getActivity(), "todo", Toast.LENGTH_SHORT).show();
             progressDialog.dismiss();
@@ -132,7 +132,32 @@ public class CreateGoalFragment extends RoboFragment {
 
         @Override
         public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-            currentTime.setText( (String.valueOf(progress + 1) + " hour") + (progress > 0 ? "s" : "") );
+
+            String hours = (String.valueOf(progress + 1) + " hour") + (progress > 0 ? "s" : "");
+
+            switch(getResources().getResourceName(seekBar.getId())) {
+                case "com.blue_stingray.healthy_life_app:id/monday_seek_bar":
+                    monday.setText(hours);
+                    break;
+                case "com.blue_stingray.healthy_life_app:id/tuesday_seek_bar":
+                    tuesday.setText(hours);
+                    break;
+                case "com.blue_stingray.healthy_life_app:id/wednesday_seek_bar":
+                    wednesday.setText(hours);
+                    break;
+                case "com.blue_stingray.healthy_life_app:id/thursday_seek_bar":
+                    thursday.setText(hours);
+                    break;
+                case "com.blue_stingray.healthy_life_app:id/friday_seek_bar":
+                    friday.setText(hours);
+                    break;
+                case "com.blue_stingray.healthy_life_app:id/saturday_seek_bar":
+                    saturday.setText(hours);
+                    break;
+                case "com.blue_stingray.healthy_life_app:id/sunday_seek_bar":
+                    sunday.setText(hours);
+                    break;
+            }
         }
 
         @Override
@@ -142,79 +167,16 @@ public class CreateGoalFragment extends RoboFragment {
         public void onStopTrackingTouch(SeekBar seekBar) {}
     }
 
-    private class DayPickerListener implements View.OnClickListener {
-
-        @Override
-        public void onClick(View v) {
-            if(days.get(v.getId()) != null && days.get(v.getId())) {
-                setDay(v, R.color.white, R.color.blue_primary);
-                days.put(v.getId(), false);
-            } else {
-                setDay(v, R.color.blue_primary, R.color.white);
-                days.put(v.getId(), true);
-            }
-        }
-    }
-
-    /**
-     * Set a single view day element colors
-     * @param v View
-     * @param background int
-     * @param textColor int
-     */
-    private void setDay(View v, int background, int textColor) {
-        GradientDrawable shape = (GradientDrawable) v.getBackground();
-        shape.setColor(getResources().getColor(background));
-        ((TextView) v).setTextColor(getResources().getColor(textColor));
-    }
-
-    /**
-     * Get a list of calendars day constants based on selected days in the UI
-     * @return ArrayList<Integer> selected days
-     */
-    private ArrayList<Integer> getSelectedDays() {
-        TextView[] dayViews = new TextView[]{
-                monday,
-                tuesday,
-                wednesday,
-                thursday,
-                friday,
-                saturday,
-                sunday
-        };
-        ArrayList<Integer> daysSelected = new ArrayList<>();
-
-        for(int i = 0; i < dayViews.length; i++) {
-            TextView day = dayViews[i];
-
-            if(days.get(day.getId()) != null && days.get(day.getId())) {
-                switch(i) {
-                    case 0:
-                        daysSelected.add(Calendar.MONDAY);
-                        break;
-                    case 1:
-                        daysSelected.add(Calendar.TUESDAY);
-                        break;
-                    case 2:
-                        daysSelected.add(Calendar.WEDNESDAY);
-                        break;
-                    case 3:
-                        daysSelected.add(Calendar.THURSDAY);
-                        break;
-                    case 4:
-                        daysSelected.add(Calendar.FRIDAY);
-                        break;
-                    case 5:
-                        daysSelected.add(Calendar.SATURDAY);
-                        break;
-                    case 6:
-                        daysSelected.add(Calendar.SUNDAY);
-                        break;
-                }
-            }
-        }
-
-        return daysSelected;
+    public HashMap<Integer, Integer> getDayHours() {
+        HashMap<Integer, Integer> dayMap = new HashMap<>();
+        dayMap.put(Calendar.MONDAY, mondaySeekBar.getProgress() + 1);
+        dayMap.put(Calendar.TUESDAY, tuesdaySeekBar.getProgress() + 1);
+        dayMap.put(Calendar.WEDNESDAY, wednesdaySeekBar.getProgress() + 1);
+        dayMap.put(Calendar.THURSDAY, thursdaySeekBar.getProgress() + 1);
+        dayMap.put(Calendar.FRIDAY, fridaySeekBar.getProgress() + 1);
+        dayMap.put(Calendar.SATURDAY, saturdaySeekBar.getProgress() + 1);
+        dayMap.put(Calendar.SUNDAY, sundaySeekBar.getProgress() + 1);
+        return dayMap;
     }
 
 }
